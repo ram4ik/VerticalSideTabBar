f56ad638b886a181db4b8128af8cbef5105d0523
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-05-14 10:24:12 +0300

    Initial Commit

---
+++ VerticalSideTabBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VerticalSideTabBar
+//
+//  Created by ramil on 14.05.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
